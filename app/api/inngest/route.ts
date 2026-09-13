cd2becef7a4a0577f2e0a4cb9eb397cc70ce3da6
Sarvesh <sarveshnirmalkar@gmail.com>
2026-09-13 11:24:58 +0530

    bug fix

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,6 +2,8 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { generateDeck } from "@/inngest/functions/generate-deck";
 
+export const maxDuration = 300;
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [generateDeck],
